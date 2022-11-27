d4ba33567ecf89601c729932d4dfd609c683750e
hichamelb <hichamelb54@gmail.com>
2022-11-27 21:50:11 +0100

    first commit

---
+++ db.js
@@ -0,0 +1,50 @@
+const mysql = require("mysql2/promise");
+
+async function start() {
+  const connection = await mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "nodeuser",
+    password: "qwertyqwerty",
+    database: "nodeapp",
+    multipleStatements: true,
+  });
+  shapeDatabase(connection);
+  module.exports = connection;
+  const app = require("./app");
+  app.listen(3000);
+}
+
+start();
+
+// Create tables and columns if they do not already exist.
+function shapeDatabase(db) {
+  db.query(`CREATE TABLE IF NOT EXISTS users (
+  _id int NOT NULL AUTO_INCREMENT,
+  username varchar(45) DEFAULT NULL,
+  email varchar(100) DEFAULT NULL,
+  password varchar(200) DEFAULT NULL,
+  avatar varchar(100) GENERATED ALWAYS AS (concat(_utf8mb4'https://gravatar.com/avatar/',md5(email),_utf8mb4'?s=128')) VIRTUAL,
+  PRIMARY KEY (_id)
+);
+CREATE TABLE IF NOT EXISTS posts (
+  _id int NOT NULL AUTO_INCREMENT,
+  title mediumtext NOT NULL,
+  body longtext NOT NULL,
+  author int NOT NULL,
+  createdDate datetime NOT NULL,
+  PRIMARY KEY (_id),
+  KEY author_fk_idx (author),
+  FULLTEXT KEY search_idx (title,body),
+  CONSTRAINT author_fr FOREIGN KEY (author) REFERENCES users (_id)
+);
+CREATE TABLE IF NOT EXISTS follows (
+  followedId int NOT NULL,
+  authorId int NOT NULL,
+  PRIMARY KEY (followedId,authorId),
+  KEY authorid_fk_idx (authorId),
+  CONSTRAINT authorid_fk FOREIGN KEY (authorId) REFERENCES users (_id),
+  CONSTRAINT followed_fk FOREIGN KEY (followedId) REFERENCES users (_id)
+);
+`);
+}
---
+++ models/Follow.js
@@ -0,0 +1,153 @@
+const db = require("../db");
+const User = require("./User");
+
+let Follow = function (followedUsername, authorId) {
+  this.followedUsername = followedUsername;
+  this.authorId = authorId;
+  this.errors = [];
+};
+
+Follow.prototype.cleanUp = async function () {
+  if (typeof this.followedUsername != "string") {
+    this.followedUsername = "";
+  }
+};
+
+Follow.prototype.validate = async function (action) {
+  /*===============================================
+  Task #1 SEE IF USERNAME EXISTS
+  You'll need: this.followedUsername
+  ===============================================*/
+  const [followedAccount] = await db.execute("SELECT * FROM users WHERE username = ?", [this.followedUsername]);
+  if (followedAccount.length) {
+    this.followedId = followedAccount[0]._id;
+  } else {
+    this.errors.push("You cannot follow a user that does not exist.");
+  }
+
+  /*===============================================
+  Task #2 SEE IF THIS USER IS ALREADY FOLLOWING THIS USER
+  You'll need: this.followedId, this.authorId
+  ===============================================*/
+  const [doesFollowAlreadyExist] = await db.execute("SELECT * FROM follows WHERE followedId = ? AND authorId = ?", [this.followedId, this.authorId]);
+  if (action == "create") {
+    if (doesFollowAlreadyExist.length) {
+      this.errors.push("You are already following this user.");
+    }
+  }
+  if (action == "delete") {
+    if (!doesFollowAlreadyExist.length) {
+      this.errors.push("You cannot stop following someone you do not already follow.");
+    }
+  }
+
+  // should not be able to follow yourself
+  if (this.followedId == this.authorId) {
+    this.errors.push("You cannot follow yourself.");
+  }
+};
+
+Follow.prototype.create = function () {
+  return new Promise(async (resolve, reject) => {
+    this.cleanUp();
+    await this.validate("create");
+    if (!this.errors.length) {
+      /*===============================================
+      Task #3 ADD A FOLLOW INTO THE FOLLOWS TABLE
+      You'll need: this.followedId, this.authorId
+      ===============================================*/
+      await db.execute("INSERT INTO follows (followedId, authorId) VALUES (?, ?)", [this.followedId, this.authorId]);
+      resolve();
+    } else {
+      reject(this.errors);
+    }
+  });
+};
+
+Follow.prototype.delete = function () {
+  return new Promise(async (resolve, reject) => {
+    this.cleanUp();
+    await this.validate("delete");
+    if (!this.errors.length) {
+      /*===============================================
+      Task #4 DELETE A FOLLOW FROM THE FOLLOWS TABLE
+      You'll need: this.followedId, this.authorId
+      ===============================================*/
+      await db.execute("DELETE FROM follows WHERE followedId = ? AND authorId = ?", [this.followedId, this.authorId]);
+      resolve();
+    } else {
+      reject(this.errors);
+    }
+  });
+};
+
+Follow.isVisitorFollowing = async function (followedId, visitorId) {
+  /*===============================================
+  Task #5 IS THE LOGGED IN USER FOLLOWING THIS USER?
+  You'll need: followedId, visitorId
+  ===============================================*/
+  const [follows] = await db.execute("SELECT * FROM follows WHERE followedId = ? AND authorId = ?", [followedId, visitorId]);
+  if (follows.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Follow.getFollowersById = function (id) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      /*===============================================
+      Task #6 GET THE USERS WHO FOLLOW THIS USER
+      You'll need: id
+      ===============================================*/
+      let [followers] = await db.execute("SELECT username, avatar FROM follows JOIN users ON follows.authorId = users._id WHERE followedId = ?", [
+        id,
+      ]);
+      resolve(followers);
+    } catch {
+      reject();
+    }
+  });
+};
+
+Follow.getFollowingById = function (id) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      /*===============================================
+      Task #7 GET THE USERS THAT THIS USER FOLLOWS
+      You'll need: id
+      ===============================================*/
+      let [followers] = await db.execute("SELECT username, avatar FROM follows JOIN users ON follows.followedId = users._id WHERE authorId = ?", [
+        id,
+      ]);
+      resolve(followers);
+    } catch {
+      reject();
+    }
+  });
+};
+
+Follow.countFollowersById = function (id) {
+  return new Promise(async (resolve, reject) => {
+    /*===============================================
+    Task #8 COUNT HOW MANY PEOPLE FOLLOW THIS USER
+    You'll need: id
+    ===============================================*/
+    const [[{ followers }]] = await db.execute("SELECT COUNT(followedId) as followers FROM follows WHERE followedId = ?", [id]);
+    resolve(followers);
+  });
+};
+
+Follow.countFollowingById = function (id) {
+  return new Promise(async (resolve, reject) => {
+    /*===============================================
+    Task #9 COUNT HOW MANY PEOPLE THIS USER FOLLOWS
+    You'll need: id
+    ===============================================*/
+    const [[{ following }]] = await db.execute("SELECT COUNT(followedId) as following FROM follows WHERE authorId = ?", [id]);
+    resolve(following);
+  });
+};
+
+module.exports = Follow;
